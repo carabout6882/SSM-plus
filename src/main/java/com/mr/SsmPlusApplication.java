e9a25dfd480ff8941102ca6fed73e0ddb19034d1
carabout6882 <2075170577@qq.com>
2022-09-02 01:02:13 +0800

    src1 commit

---
+++ src/main/java/com/mr/SsmPlusApplication.java
@@ -0,0 +1,14 @@
+package com.mr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SsmPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmPlusApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mr/service/BookService.java
@@ -0,0 +1,16 @@
+package com.mr.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.mr.domain.Book;
+
+
+import java.util.List;
+
+public interface BookService {
+    Boolean save(Book book);
+    Boolean update(Book book);
+    Boolean delete(Integer id);
+    Book getById(Integer id);
+    List<Book> getAll();
+    IPage<Book> getPage(int currentPage,int pageSize);
+}
